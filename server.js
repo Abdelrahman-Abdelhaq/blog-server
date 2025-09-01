c18e0c527a6b676aef7cf4be14149ca2af57b5b2
Abdelrahman Alaa <abdelrahmanalaa.psn@gmail.com>
2025-09-01 12:58:53 +0300

    apis

--- server.js
+++ server.js
@@ -16,6 +16,27 @@ app.get('/posts',async (req,res)=>{
     res.json(result.rows);
 })      
 
+app.post("/posts" , async (req,res)=>{
+    const {post_category,post_title,post_description} = req.body;
+    const result = await pool.query("insert into post(post_category,post_title,post_description) values($1,$2,$3) returning *",[post_category,post_title,post_description])
+    res.json(result.rows[0])
+    res.json("New Post Added Successfully!")
+
+})
+
+app.delete('/posts/:id',async (req,res) =>{
+    const {id} = req.params;
+    const result = await pool.query("delete from post where post_id=$1",[id])
+    res.json("Post Deleted Successfully!")
+})
+
+app.put("/posts/:id", async(req,res)=>{
+    const {id} = req.params;
+    const {post_category,post_title,post_description} = req.body;
+    const result = await pool.query("UPDATE post SET post_category = $1, post_title = $2, post_description=$3 where post_id = $4",[post_category,post_title,post_description,id])
+    res.json("Post Updated Successfully!")
+})
+
 app.listen(port, ()=>{
     console.log(`Listening on Port ${port}`)
 })
