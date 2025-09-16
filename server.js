fafdb851647213f2e05517e499ac2831bb5e97fd
Abdelrahman Alaa <abdelrahmanalaa.psn@gmail.com>
2025-09-16 11:20:21 +0300

    clean code

--- server.js
+++ server.js
@@ -2,40 +2,22 @@ import express from "express"
 import cors from "cors"
 import  dotenv from "dotenv/config"
 import pool from "./db.js";
+import { addPost, deletePost, getPosts, updatePost } from "./controllers.js";
 
-const port = process.env.ball|| 4000;
+const port = process.env.port|| 4000;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json())
 
-app.get('/posts',async (req,res)=>{
-    const {limit,offset} = req.query;
-    const result = await pool.query(`select * from post order by post_id asc limit ${limit} offset ${offset} `)
-    res.json(result.rows);
-})      
+app.get('/posts',getPosts)      
 
-app.post("/posts" , async (req,res)=>{
-    const {post_category,post_title,post_description} = req.body;
-    const result = await pool.query("insert into post(post_category,post_title,post_description) values($1,$2,$3) returning *",[post_category,post_title,post_description])
-    res.json(result.rows[0])
-    res.json("New Post Added Successfully!")
+app.post("/posts" , addPost)
 
-})
+app.delete('/posts/:id',deletePost)
 
-app.delete('/posts/:id',async (req,res) =>{
-    const {id} = req.params;
-    const result = await pool.query("delete from post where post_id=$1",[id])
-    res.json("Post Deleted Successfully!")
-})
-
-app.put("/posts/:id", async(req,res)=>{
-    const {id} = req.params;
-    const {post_category,post_title,post_description} = req.body;
-    const result = await pool.query("UPDATE post SET post_category = $1, post_title = $2, post_description=$3 where post_id = $4",[post_category,post_title,post_description,id])
-    res.json("Post Updated Successfully!")
-})
+app.put("/posts/:id", updatePost)
 
 app.listen(port, ()=>{
     console.log(`Listening on Port ${port}`)
