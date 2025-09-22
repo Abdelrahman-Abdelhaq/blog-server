3d751297c877d4bf7970fbf19e25739831cc5014
Abdelrahman Alaa <abdelrahmanalaa.psn@gmail.com>
2025-09-22 09:14:18 +0300

    login and signup apis

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express"
 import cors from "cors"
 import  dotenv from "dotenv/config"
-import { addComment, addPost, deletePost, getComments, getPost, getPosts, /*updatePost*/ } from "./controllers.js";
+import { addComment, addPost, addUser, deletePost, getComments, getPost, getPosts, userLogin, /*updatePost*/ } from "./controllers.js";
 import pool from "./db.js";
 
 const port = process.env.port|| 4000;
@@ -18,6 +18,9 @@ app.get('/postpage/:id' , getComments)
 
 app.post("/posts" , addPost)
 app.post('/postpage/:id', addComment)
+app.post('/signup', addUser)
+app.post('/', userLogin)
+
 
 
 app.delete('/posts/:id',deletePost)
@@ -28,20 +31,8 @@ app.put('/postpage', async(req,res) => {
     res.json(result.rows[0])
 })
 
-// app.put("/posts/:id", updatePost)
 app.listen(port, ()=>{
     console.log(`Listening on Port ${port}`)
 })
 
-// so now we need 3 apis 
-// first one is a post api that will be used when we add a new comment , it will send
-// the comment id ,username, and default false liked state
-
-// then one to fetch all  the data from the comments_likes table and it will be fetched on the parent 
-// to an array object
-
-// then pass it as prop to the children comments
-// now for each children you will use if statement to check for the username comment id and liked state
-// first 2 matches and the last one is false then we can like if not then we cant 
-
-// when we like we increase the likes number by one and change the image and state
+// app.put("/posts/:id", updatePost)
