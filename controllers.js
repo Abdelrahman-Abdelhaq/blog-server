82c64ff5140cd5b2baa55f8563dea6ab91bedd7b
Abdelrahman Alaa <abdelrahmanalaa.psn@gmail.com>
2025-09-22 09:14:45 +0300

    controllers/handlers for  login and signup

--- controllers.js
+++ controllers.js
@@ -1,5 +1,6 @@
-import { addingCommentQ, addingPostQ, deleteingPostQ, gettingComments, gettingPostQ, gettingPostsQ, /*updatingPostQ*/ } from "./queries.js";
+import { addingCommentQ, addingPostQ, addingUserQ, deleteingPostQ, gettingComments, gettingPostQ, gettingPostsQ, userLoginQ, /*updatingPostQ*/ } from "./queries.js";
 import pool from "./db.js";
+import bcrypt from "bcrypt"
 
 export const getPosts = async (req,res)=>{
     const {limit,offset} = req.query;
@@ -34,6 +35,42 @@ export const addComment = async (req,res) => {
     const {comment_paragraph} = req.body
     const result = await pool.query(addingCommentQ,[comment_paragraph,id])
     res.json(result.rows[0])
+}
+
+export const addUser = async (req, res) => {
+    const user = req.body.user.toLowerCase();
+    const mail = req.body.mail.toLowerCase();
+    const pass = req.body.pass;
+    try {
+        const saltRounds = 10
+        const hashedPassword = await bcrypt.hash(pass,saltRounds)
+        const result = await pool.query(addingUserQ,[user,mail,hashedPassword])
+        res.status(200).json('Signed Up Successfully')
+    } catch (err) {
+        if(err.code === "23505") {
+        res.status(409).json("Username Already Exists")
+        }else{
+        console.error(err)
+        res.status(500).json("Internal Server Error!")
+        }
+        
+    }
+} 
+
+export const userLogin = async (req, res) => {
+    const mail = req.body.mail.toLowerCase()
+    const pass = req.body.pass
+    try {
+        const result = await pool.query(userLoginQ,[mail,pass])
+        if(result.rowCount > 0 ){
+            res.status(200).json("Login Successful")
+        }else{
+            res.status(401).json("Login Failed")
+        }
+    } catch (error) {
+        console.error(error)
+        res.status(500).json("Server Error")
+    }
 }
 
 export const deletePost = async (req,res) =>{
